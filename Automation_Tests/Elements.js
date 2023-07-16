202b128e8ad12257857f6034e4c7ecbb29becce7
Arsen Buhaienko <arsenbuhaienko@gmail.com>
2023-07-17 00:14:54 +0300

    added beforeAll annotation

--- Automation_Tests/Elements.js
+++ Automation_Tests/Elements.js
@@ -1,4 +1,24 @@
-import { checkSectionTextBox } from './DemoQA.spec.js';
-const { test, expect } = require('@playwright/test');
+// @ts-check
 
-console.log (checkSectionTextBox());
+const { test } = require('@playwright/test');
+
+test.describe.configure({ mode: 'serial' });
+
+/** @type {import('@playwright/test').Page} */
+let page;
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
+});
+
+test.afterAll(async () => {
+  await page.close();
+});
+
+test('runs first', async () => {
+  await page.goto('https://playwright.dev/');
+});
+
+test('runs second', async () => {
+  await page.getByText('Get Started').click();
+});

02fa3a348423ca6d39182259374ee0bd7037a510
Arsen Buhaienko <arsenbuhaienko@gmail.com>
2023-07-17 21:11:12 +0300

    Main Structure

--- Automation_Tests/test-1.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://demoqa.com/');
-  await page.locator('svg').first().click();
-  await page.getByText('Check Box').click();
-  await page.locator('#tree-node').getByRole('img').nth(3).click();
-});
